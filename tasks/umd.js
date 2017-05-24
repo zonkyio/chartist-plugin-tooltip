c476a2dd255134241e4238f562ac3cb8b617bc79
Blake Burkett <blake.a.burkett@gmail.com>
2017-05-24 10:28:36 -0400

    fix to discrepency of 'tooltip' vs 'tooltips'

--- tasks/umd.js
+++ tasks/umd.js
@@ -14,7 +14,7 @@ module.exports = function (grunt) {
     dist: {
       src: '<%= pkg.config.src %>/scripts/<%= pkg.config.src_name %>.js',
       dest: '<%= pkg.config.dist %>/<%= pkg.config.src_name %>.js',
-      objectToExport: 'Chartist.plugins.tooltips',
+      objectToExport: 'Chartist.plugins.tooltip',
       deps: {
         default: ['Chartist'],
         amd: ['chartist'],
